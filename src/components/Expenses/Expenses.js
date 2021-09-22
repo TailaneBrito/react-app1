97c482ef1d2e1ecb685ce4763b9f9aa1d09b548e
tndbrito <tailane.brito01@gmail.com>
2021-09-21 21:47:32 -0300

    transforming array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -24,10 +24,16 @@ const Expenses = (props) => {
             <Card className="expenses">
                 <ExpensesFilter selected={filterdYear} onChangeFilter={filterChangeHandler} />
 
-                {props.items.map(expense => <ExpenseItem />)}
+                {props.items.map(expense => (
+                    <ExpenseItem 
+                        title={expense.title} 
+                        amount={expense.amount}
+                        date={expense.date}    
+                    />
+                ))};
                                 
 
-                <ExpenseItem 
+                {/* <ExpenseItem 
                     title={props.items[0].title}
                     amount={props.items[0].amount}
                     date={props.items[0].date}
@@ -46,7 +52,7 @@ const Expenses = (props) => {
                     title={props.items[3].title}
                     amount={props.items[3].amount}
                     date={props.items[3].date}
-                />
+                /> */}
             </Card>
         </div>
 
