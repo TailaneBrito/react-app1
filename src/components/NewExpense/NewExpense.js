2f66fd0bc2ccb11c923eeb99e0f791f58698bfe4
tndbrito <tailane.brito01@gmail.com>
2021-10-01 17:54:20 -0300

    68. Demo app Adding a Chart

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
 
-    {/** passing data to the console. **/}
     const saveExpenseDataHandler = (enteredExpenseData) => {
         {/* Pass ExpenseData to the APP, from ExpenseForm to New Expense to App**/}
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-    
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expenses</button>}
+            {isEditing && 
+                <ExpenseForm 
+                    onSaveExpenseData={saveExpenseDataHandler} 
+                    onCancel={stopEditingHandler} 
+                />}
         </div>
     );
 };
